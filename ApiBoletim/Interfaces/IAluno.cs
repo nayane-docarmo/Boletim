820af8c887f2b8b5c2097f7072ccba59cec1ddd8
nayane-docarmo <nayane.docarmo02@gmail.com>
2020-12-15 11:35:41 -0300

    ApiAluno Concluida

---
+++ ApiBoletim/Interfaces/IAluno.cs
@@ -0,0 +1,17 @@
+﻿using ApiBoletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiBoletim.Interfaces
+{
+    interface IAluno
+    {
+        Aluno cadastrar(Aluno a);
+        List<Aluno> LerTodos();
+        Aluno BuscarPorId(int id);
+        Aluno Alterar(Aluno a);
+        Aluno Excluir(Aluno a);
+    }
+}
---
+++ ApiBoletim/Repositories/AlunoRepository.cs
@@ -0,0 +1,37 @@
+﻿using ApiBoletim.Domains;
+using ApiBoletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiBoletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+        public Aluno Alterar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
